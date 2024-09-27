c80e36f02660153a2662dabf3d9d07ed0125a849
matthewkwapyong <kwapyong@gmail.com>
2024-09-27 23:15:48 +0100

    new login and sign up ui

--- estos/src/app/(auth)/login/page.js
+++ estos/src/app/(auth)/login/page.js
@@ -2,43 +2,72 @@
 import styles from "../login.module.css";
 import { useRouter } from 'next/navigation'
 import { Login } from '@/lib'
-import { useActionState } from 'react'
+import { useState } from 'react'
+import { useFormState } from 'react-dom'
+
+import { Open, Close } from '../../../../Components/eye'
 import Link from "next/link";
+
+const initialState = {
+    authenticated: true,
+}
 export default function Home() {
+    let [showpass, setshowpass] = useState(false)
+    const [state, formAction] = useFormState(Login, initialState)
+    let showpassword = () => setshowpass(!showpass)
+    useState(() => {
+        console.log(state)
+    })
     return (
         <div className={styles.App}>
             <div className={styles.Container + " " + styles.login}>
                 <div className={styles.top}>
                     <label>Create Account</label>
                 </div>
+                <div className={styles.errorContainer} style={!state.authenticated ? { display: "flex" } : { display: "none" }}>
+                    <div >
+                        {state.type == 0 ?
+                            <label>account doesn't exists</label>
+                            :
+                            <label>wrong password</label>
+                        }
+                    </div>
+                </div>
                 <div id={styles.fail}>
                     <label>Email exists</label>
                 </div>
                 <div className={styles.form}>
-                    <form id={styles.logg} action={Login}>
-                        <div className={styles.field}>
-                            <div>
-                                <label>username</label>
+                    <form id={styles.logg} action={formAction}>
+                        <div className={styles.formItems}>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>username</label>
+                                </div>
+                                <input type="text" name="username" required style={{ width: "340px" }} />
                             </div>
-                            <input type="text" name="username" required />
-                        </div>
-                        <div className={styles.field}>
-                            <div>
-                                <label>Password</label>
-                            </div>
-                            <div className={styles.passwordContainer}>
-                                <input
-                                    id={styles.pass}
-                                    type="password"
-                                    name="password"
-                                    required
-                                />
-                                <button
-                                    id={styles.showpass}
-                                    type="button"
-                                // onClick="showPassword(event)"
-                                >
-                                </button>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>Password</label>
+                                </div>
+                                <div className={styles.passwordContainer} style={{ width: "340px" }}>
+                                    <input
+                                        id={styles.pass}
+                                        type="password"
+                                        name="password"
+                                        required
+                                    />
+                                    <button
+                                        id={styles.showpass}
+                                        type="button"
+                                        onClick={showpassword}
+                                    >
+                                        {showpass ?
+                                            <Close />
+                                            :
+                                            <Open />
+                                        }
+                                    </button>
+                                </div>
                             </div>
                         </div>
                         <div className={styles.submit}>
@@ -47,9 +76,10 @@ export default function Home() {
                     </form>
                 </div>
                 <div className={styles.next}>
-                    <Link href="signup" >Login</Link>
+                    <Link href="signup">Create account</Link>
                 </div>
             </div>
         </div>
     );
 }
+
--- estos/src/app/(auth)/signup/page.js
+++ estos/src/app/(auth)/signup/page.js
@@ -1,70 +1,86 @@
 'use client'
 import styles from "../login.module.css";
 import { useRouter } from 'next/navigation'
-import {Signup} from '@/lib'
-import {useActionState} from 'react'
+import { Signup } from '@/lib'
+import { useActionState, useEffect, useState } from 'react'
+import { useFormState } from 'react-dom'
+import { Open, Close } from '../../../../Components/eye'
+
 import Link from "next/link";
+const initialState = {
+    exists: false,
+}
 export default function Home() {
-    const router = useRouter()
-    let move = (e) => {
-        console.log("dad")
-    }
+    const [state, formAction] = useFormState(Signup, initialState)
+    let [showpass, setshowpass] = useState(false)
+    let showpassword = () => setshowpass(!showpass)
     return (
         <div className={styles.App}>
             <div className={styles.Container + " " + styles.signup}>
                 <div className={styles.top}>
                     <label>Create Account</label>
                 </div>
+                <div className={styles.errorContainer} style={state.exists ? { display:"flex" } : {display:"none" }}>
+                    <div >
+                        <label>Email or username exists</label>
+                    </div>
+                </div>
                 <div id={styles.fail}>
-                    <label>Email exists</label>
                 </div>
                 <div className={styles.form}>
-                    <form id={styles.logg} action={Signup}>
-                        <div className={styles.field}>
-                            <div>
-                                <label>First Name</label>
+                    <form id={styles.logg} action={formAction}>
+                        <div className={styles.formItems}>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>First Name</label>
+                                </div>
+                                <input type="text" name="first_name" required />
                             </div>
-                            <input type="text" name="first_name" required />
-                        </div>
-                        <div className={styles.field}>
-                            <div>
-                                <label>Last Name</label>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>Last Name</label>
+                                </div>
+                                <input type="text" name="last_name" required />
                             </div>
-                            <input type="text" name="last_name" required />
-                        </div>
-                        <div className={styles.field}>
-                            <div>
-                                <label>username</label>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>username</label>
+                                </div>
+                                <input type="text" name="username" required />
                             </div>
-                            <input type="text" name="username" required />
-                        </div>
-                        <div className={styles.field}>
-                            <div>
-                                <label>Email</label>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>Email</label>
+                                </div>
+                                <input type="email" name="email" required />
                             </div>
-                            <input type="email" name="email" required />
-                        </div>
-                        <div className={styles.field}>
-                            <div>
-                                <label>Password</label>
+                            <div className={styles.field}>
+                                <div>
+                                    <label>Password</label>
+                                </div>
+                                <div className={styles.passwordContainer}>
+                                    <input
+                                        id={styles.pass}
+                                        type={showpass ? "" : "password"}
+                                        name="password"
+                                        required
+                                    />
+                                    <button
+                                        id={styles.showpass}
+                                        type="button"
+                                        onClick={showpassword}
+                                    >
+                                        {showpass ?
+                                            <Open />
+                                            :
+                                            <Close />
+                                        }
+                                    </button>
+                                </div>
                             </div>
-                            <div className={styles.passwordContainer}>
-                                <input
-                                    id={styles.pass}
-                                    type="password"
-                                    name="password"
-                                    required
-                                />
-                                <button
-                                    id={styles.showpass}
-                                    type="button"
-                                // onClick="showPassword(event)"
-                                >
-                                </button>
+                            <div className={styles.submit}>
+                                <button type="submit">Create</button>
                             </div>
-                        </div>
-                        <div className={styles.submit}>
-                            <button type="submit">Create</button>
                         </div>
                     </form>
                 </div>
@@ -75,3 +91,4 @@ export default function Home() {
         </div>
     );
 }
+
