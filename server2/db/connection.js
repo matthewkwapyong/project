4135a86a21e846e7f9c82dfb8310e771665b8d34
matthewkwapyong <kwapyong@gmail.com>
2024-12-31 00:59:51 +0100

    env

--- server2/db/connection.js
+++ server2/db/connection.js
@@ -1,26 +1,25 @@
-
 let pg = require("pg")
-const { Client,Pool } = pg
+const { Client, Pool } = pg
 const client = new Client()
 
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'matthew',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'estos',
+//   })
 
 
-const pool = new Pool({
-    user: 'postgres',
-    password: 'matthew',
-    host: 'localhost',
-    port: 5432,
-    database: 'estos',
-  })
+pool = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD
+});
 
 
 module.exports = pool
 
-async function getUser(id){
-
-    const res = await pool.query('SELECT username from users where username = $1', [id])
-    return res.rows[0]
-}
-async function createUser(data){
-
-}
